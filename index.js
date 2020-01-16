e1f13c483da4c862917f8d3ab44272088eca7519
Ryan Soto <rsoto003@ucr.edu>
2020-01-16 12:15:57 -0800

    finished record collection algorithm

--- index.js
+++ index.js
@@ -80,4 +80,62 @@ var myObj = {
     }
     // Add record here
   ];
+  /* Accessing Nested Objects */
+  //Sub properties of objects can be accessed by chaining together the dot or bracket notation.
+
+  /* Accessing Nested Arrays*/  
+  //Array bracket notation can be chained to acces nested arrays.  
+
+  /* * Record Collection * */
+
+var collection = {
+    2548: {
+      album: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: [
+        "Let It Rock",
+        "You Give Love a Bad Name"
+      ]
+    },
+    2468: {
+      album: "1999",
+      artist: "Prince",
+      tracks: [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    1245: {
+      artist: "Robert Palmer",
+      tracks: [ ]
+    },
+    5439: {
+      album: "ABBA Gold"
+    }
+  };
   
+  function updateRecords(id, prop, value) {
+    if(prop !== "tracks" && value !== ""){
+      collection[id][prop] = value;
+    }
+    if(prop == "tracks" && !collection[id].hasOwnProperty("tracks")){
+      collection[id]["tracks"] = [];
+    }
+    if(prop == "tracks" && value !== ""){
+      collection[id]["tracks"].push(value);
+    }
+    if(value === ""){
+      delete collection[id][prop];
+    }
+  
+    return collection;
+  }
+  
+  // Test Cases
+  updateRecords(5439, "artist", "ABBA");
+  updateRecords(5439, "tracks", "Take a Chance on Me");
+  updateRecords(2548, "artist", "");
+  updateRecords(1245, "tracks", "Addicted to Love");
+  updateRecords(2468, "tracks", "Free");
+  updateRecords(2548, "tracks", "");
+  updateRecords(1245, "album", "Riptide");
