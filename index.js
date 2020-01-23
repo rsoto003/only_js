5a147c9407ee546aceadad2e64fb871c7d2e74e6
Ryan Soto <rsoto003@ucr.edu>
2020-01-23 11:38:38 -0800

    finished profile lookup function

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ var collection = {
 
 
    /* Replace Loops using Recursion */
-   /**
+   /*
     * Recursion is the concept that a function can be expressed in terms of itself
     * 
     * 
@@ -199,3 +199,50 @@ var collection = {
         return sum(arr, n - 1) + arr[n];
       }
   }
+
+  /* Profile Lookup */
+
+  var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+    for(var index = 0; index < contacts.length; index++){
+        if(contacts[index]["firstName"] === name){
+            if(contacts[index].hasOwnProperty(prop)){
+                return contacts[index][prop];
+            } else {
+                return "No such property"
+            }
+        }
+    } 
+    return "No such contact"
+}
+
+lookUpProfile("Akira", "likes");
+lookUpProfile("Sherlock", "likes");
+lookUpProfile("Harry","likes");
