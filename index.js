6721dc338d6de396c1450283cd0f2a76aeb5cc7e
Ryan Soto <rsoto003@ucr.edu>
2020-01-27 23:58:52 -0800

    testing destructing

--- index.js
+++ index.js
@@ -291,3 +291,15 @@ function rangeOfNumbers(startNum, endNum) {
     }
   }
   
+/* Destructuring Assignment to Extract Values from Objects */
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  const {today, tomorrow} = HIGH_TEMPERATURES;
+
+  console.log(yesterday) // should be not defined
+  console.log(today); // should be 77
+  console.log(tomorrow); // should be 80
